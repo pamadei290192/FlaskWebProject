98657c19c8f4a26923e51d177dbded418e39bc35
Phil AMD <philippe.amadei@yahoo.com>
2019-05-30 16:11:41 +0200

    Page management

--- HelloFlask/views.py
+++ HelloFlask/views.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 from HelloFlask import app
+import datetime as dt
 
 @app.route('/')
 @app.route('/home')
+
 def home():
-    return "Hello Flask!"
+    now = dt.datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = " on " + formatted_now)
